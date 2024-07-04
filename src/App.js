ea80d24de988b640f64d8eb90c7fe1878a6d9409
Amir Eftekhar <amir.hossein.eftekhar@gmail.com>
2024-07-03 20:04:23 -0700

    _

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Web from './page.jsx'
 import ProjectsPage from './projects.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 function App() {
+  const basePath = process.env.NODE_ENV === 'production' ? '/protosync-webcite' : '';
+
   return (
-    <Router>
+    <Router basename={basePath}>
       <Routes>
         <Route path="/" element={<Web />} />
         <Route path="/projects" element={<ProjectsPage />} />
-
       </Routes>
     </Router>
   );
 }
+
 export default App;
--- src/meet.jsx
+++ src/meet.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion} from 'framer-motion';
 
 const TypeWriter = ({ text, delay = 40 }) => {
   const [displayText, setDisplayText] = useState('');
